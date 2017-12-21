cb64e9e472fd3510600f3fb6b9fe570372c81d5f
Kaplan Lukas <kaplan@RTLS.dragon.cz>
2017-12-21 14:43:29 +0100

    update of comments

--- rtls_server.php
+++ rtls_server.php
@@ -116,30 +116,34 @@ $conn->close();
 /****************************************************************************************************/
 
 function get_msg_type($header){
-	$type = substr($header,0,2);
-	$type = bin2hex($type);
-	if($type == "0000") return "AR_AS_CONFIG_SET";
-	if($type == "0001") return "AR_STATION_REQUEST";
-	if($type == "0010") return "AR_ACK";
-	if($type == "0011") return "AR_NACK";
-	if($type == "0012") return "AR_TAG_REPORT";
-	if($type == "0013") return "AR_STATION_REPORT";
-	if($type == "0014") return "AR_COMPOUND_MESSAGE_REPORT";
-	if($type == "0015") return "AR_AP_NOTIFICATION";
-	if($type == "0016") return "AR_MMS_CONFIG_SET";
-	if($type == "0017") return "AR_STATION_EX_REPORT";
-	if($type == "0018") return "AR_AP_EX_REPORT";
-	else return "UNKNOWN";
+    /*
+     * $header = 16 byte binary RTLS header
+     * return string - name/type of the message
+     */
+    $type = substr($header,0,2);
+    $type = bin2hex($type);
+    if($type == "0000") return "AR_AS_CONFIG_SET";
+    if($type == "0001") return "AR_STATION_REQUEST";
+    if($type == "0010") return "AR_ACK";
+    if($type == "0011") return "AR_NACK";
+    if($type == "0012") return "AR_TAG_REPORT";
+    if($type == "0013") return "AR_STATION_REPORT";
+    if($type == "0014") return "AR_COMPOUND_MESSAGE_REPORT";
+    if($type == "0015") return "AR_AP_NOTIFICATION";
+    if($type == "0016") return "AR_MMS_CONFIG_SET";
+    if($type == "0017") return "AR_STATION_EX_REPORT";
+    if($type == "0018") return "AR_AP_EX_REPORT";
+    else return "UNKNOWN";
 }
 
 function rcv_explode($message){
-	/* 
-	 * $message = whole RTLS binary message
-	 * return array:
-	 * [0] = 16 byte - RTLS header
-	 * [1] = RTLS payload
-	 * [2] = 20 byte - RTLS hmac-sha1 hash of header and payload
-	 */
+    /*
+     * $message = whole RTLS binary message
+     * return array:
+     * [0] = 16 byte - RTLS header
+     * [1] = RTLS payload
+     * [2] = 20 byte - RTLS hmac-sha1 hash of header and payload
+     */
     $rtls = array();
     $rtls[0] = substr($message, 0, 16);
     $rtls[1] = substr($message, 16, -20);
@@ -148,17 +152,17 @@ function rcv_explode($message){
 }
 
 function parse_header($header){
-	/*
-	 * $header = 16 byte header in binary
-	 * return array
-	 * [0] = 2 byte - Message type
-	 * [1] = 2 byte - Message Id
-	 * [2] = 1 byte - Major version (1 or 2)
-	 * [3] = 1 byte - Minor version (always 0)
-	 * [4] = 2 byte - Data Length (length of rtls payload)
-	 * [5] = 6 byte - AP MAC
-	 * [6] = 2 byte - Padding
-	 */
+    /*
+     * $header = 16 byte header in binary
+     * return array
+     * [0] = 2 byte - Message type
+     * [1] = 2 byte - Message Id
+     * [2] = 1 byte - Major version (1 or 2)
+     * [3] = 1 byte - Minor version (always 0)
+     * [4] = 2 byte - Data Length (length of rtls payload)
+     * [5] = 6 byte - AP MAC
+     * [6] = 2 byte - Padding
+     */
     $field = array();
     $field[0] = substr($header,0,2);
     $field[1] = substr($header,2,2);
@@ -171,30 +175,30 @@ function parse_header($header){
 }
 
 function parse_stationreport($payload){
-	/*
-	 * $payload = 28 byte binary ar_station_report message
-	 * Return array:
-	 * [0] = 6 byte - station MAC (ap or client)
-	 * [1] = 1 byte - noise floor
-	 * [2] = 1 byte - Data rate
-	 * [3] = 1 byte - Channel
-	 * [4] = 1 byte - RSSI
-	 * [5] = 1 byte - Type (AR_WLAN_CLIENT, AR_WLAN_AP)
-	 * [6] = 1 byte - Associated (1= all aps and associated stations, 2= unassociated stations)
-	 * [7] = 6 byte - Radio_BSSID (radio which detected the device)
-	 * [8] = 6 byte - Mon_BSSID (AP that the station is associated to)
-	 * [9] = 4 byte - age, # of seconds since the last packet was heard from this station
-	 * */
-	$field = array();
-	$field[0] = substr($payload,0,6);
-	$field[1] = substr($payload,6,1);
-	$field[2] = substr($payload,7,1);
-	$field[3] = substr($payload,8,1);
-	$field[4] = substr($payload,9,1);
-	$field[5] = substr($payload,10,1);
-	$field[6] = substr($payload,11,1);
-	$field[7] = substr($payload,12,6);
-	$field[8] = substr($payload,18,6);
-	$field[9] = substr($payload,24,4);
-	return $field;
+    /*
+     * $payload = 28 byte binary ar_station_report message
+     * Return array:
+     * [0] = 6 byte - station MAC (ap or client)
+     * [1] = 1 byte - noise floor
+     * [2] = 1 byte - Data rate
+     * [3] = 1 byte - Channel
+     * [4] = 1 byte - RSSI
+     * [5] = 1 byte - Type (AR_WLAN_CLIENT, AR_WLAN_AP)
+     * [6] = 1 byte - Associated (1= all aps and associated stations, 2= unassociated stations)
+     * [7] = 6 byte - Radio_BSSID (radio which detected the device)
+     * [8] = 6 byte - Mon_BSSID (AP that the station is associated to)
+     * [9] = 4 byte - age, # of seconds since the last packet was heard from this station
+     */
+    $field = array();
+    $field[0] = substr($payload,0,6);
+    $field[1] = substr($payload,6,1);
+    $field[2] = substr($payload,7,1);
+    $field[3] = substr($payload,8,1);
+    $field[4] = substr($payload,9,1);
+    $field[5] = substr($payload,10,1);
+    $field[6] = substr($payload,11,1);
+    $field[7] = substr($payload,12,6);
+    $field[8] = substr($payload,18,6);
+    $field[9] = substr($payload,24,4);
+    return $field;
 }
